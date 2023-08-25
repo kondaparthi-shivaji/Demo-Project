68f67a067fc7250fa382d9fe337054ac389fb139
kondaparthi-shivaji <kondaparthishivaji04@gmail.com>
2023-08-25 12:42:33 +0530

    Project Changes are commiting

---
+++ DemoProjectPOC/DemoConfig/ServiceLayer/EmpDataServiceLayer.cs
@@ -0,0 +1,60 @@
+﻿using DemoConfig.Repository;
+using DemoModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConfig.ServiceLayer
+{
+    public class EmpDataServiceLayer
+    {
+        public EmpDataResponse EmployeeData(EmpPostData empPostData)
+        {
+            try
+            {
+                return new EmpDataRepositoryLayer().EmployeeData(empPostData);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public GetEmpResponse GetEmployeeData()
+        {
+            try
+            {
+                return new EmpDataRepositoryLayer().GetEmployeeData();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public EmpDataResponse UpdateEmployeeData(UpdateEmpData updateEmpData)
+        {
+            try
+            {
+                return new EmpDataRepositoryLayer().UpdateEmployeeData(updateEmpData);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public EmpDataResponse DeleteEmployeeData(DeleteEmpData deleteEmpData)
+        {
+            try
+            {
+                return new EmpDataRepositoryLayer().DeleteEmployeeData(deleteEmpData);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ DemoProjectPOC/DemoModels/Models/GetEmpData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoModels.Models
+{
+    public class GetEmpResponse
+    {
+        public List<GetEmpData> listEmployeeDetails { get; set; }
+    }
+    public class GetEmpData
+    {
+        public int? EmpNo { get; set; }
+        public string EmpFirstName { get; set; }
+        public string EmpLastName { get; set; }
+        public string EmpDesignation { get; set; }
+        public string EmpDomain { get; set; }
+        public string EmpSalary { get; set; }
+        public DateTime? EmpDOB { get; set; }
+        public string ActiveFlag { get; set; }
+    }
+}
