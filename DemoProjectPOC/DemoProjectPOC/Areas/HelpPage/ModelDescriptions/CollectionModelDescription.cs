5bf6b0aa3cda5ff6df07493c5aa045ce03ceb7f5
kondaparthi-shivaji <kondaparthishivaji04@gmail.com>
2023-08-25 12:14:48 +0530

    Initial Commit

---
+++ DemoProjectPOC/DemoProjectPOC/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DemoProjectPOC.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
